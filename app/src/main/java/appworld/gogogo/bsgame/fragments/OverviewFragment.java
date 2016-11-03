bf4d6279b3a71d162a3ba1606bd4d03d98aa1e4a
Aaron <aaron.siegl@gmx.de>
2016-11-03 11:47:21 +0100

    fragment_overview kleiner Änderungen

--- app/src/main/java/appworld/gogogo/bsgame/fragments/OverviewFragment.java
+++ app/src/main/java/appworld/gogogo/bsgame/fragments/OverviewFragment.java
@@ -7,16 +7,20 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
 
 import appworld.gogogo.bsgame.R;
+
+import static appworld.gogogo.bsgame.R.*;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class OverviewFragment extends Fragment {
 
-    private EditText username;
+    private Spinner field_size;
     public OverviewFragment() {
         // Required empty public constructor
     }
@@ -25,12 +29,18 @@ public class OverviewFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_overview, container, false);
+        return inflater.inflate(layout.fragment_overview, container, false);
     }
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        field_size = (Spinner)view.findViewById(id.choose_fieldsize);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(), array.size_array,android.R.layout.simple_spinner_item);
+        field_size.setAdapter(adapter);
+
+
+
 
 
     }
