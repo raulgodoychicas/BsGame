c8e4d3b73226ac4b4a0bc3581047d48439b28ada
raulgodoychicas <raul_godoy_22@hotmail.com>
2016-12-14 10:07:09 +0100

    Canvas lines

--- app/src/main/java/appworld/gogogo/bsgame/engine/PlayGroundView.java
+++ app/src/main/java/appworld/gogogo/bsgame/engine/PlayGroundView.java
@@ -1,43 +1,59 @@
 package appworld.gogogo.bsgame.engine;
 
 import android.content.Context;
-import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
-import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
 import android.view.View;
-import android.view.ViewGroup;
 
 /**
  * Created by Raul on 04.11.2016.
  */
 
-public class PlayGroundView extends View implements View.OnTouchListener {
+public class PlayGroundView extends View {
 
-    private static String NUMBER_OF_SQUARES = "numberOfSquares";
+    private Paint linePaintGray, pointPaintWhite, selectedLinePaintRed;
 
-    private Paint mPaint;
-    private Rect[] rects;
-    private boolean[] markedRects;
+    private lineOnCanvas[] verticalLinesOnCanvases;
+    private lineOnCanvas[] horizontalLinesOnCanvas;
+
     private int numberOfSquares;
     private int parentWidth;
     private int parentHeight;
 
-    public PlayGroundView(Context context,int numberOfSquares){
+    private ScaleGestureDetector mScaleDetector;
+    private float mScaleFactor = 1.f;
+
+    public PlayGroundView(Context context, int numberOfSquares) {
         super(context);
         this.numberOfSquares = numberOfSquares;
 
-        //TODO: getAttribute ist nicht richtig
-        // and we set a new Paint with the desired attributes
-        mPaint = new Paint();
-        mPaint.setAntiAlias(true);
-        mPaint.setColor(Color.BLACK);
-        mPaint.setStyle(Paint.Style.STROKE);
-        mPaint.setStrokeWidth(3);
+        mScaleDetector = new ScaleGestureDetector(context, new ScaleListener());
+
+        // Define Paints
+        linePaintGray = new Paint();
+        linePaintGray.setAntiAlias(true);
+        linePaintGray.setColor(Color.GRAY);
+        linePaintGray.setStyle(Paint.Style.STROKE);
+        // material design 38% alpha for hints
+        linePaintGray.setAlpha((int) (255 * 0.38));
+        linePaintGray.setStrokeWidth(3);
+
+        pointPaintWhite = new Paint();
+        pointPaintWhite.setAntiAlias(true);
+        pointPaintWhite.setColor(Color.WHITE);
+        pointPaintWhite.setStyle(Paint.Style.STROKE);
+        pointPaintWhite.setStrokeWidth(9);
+
+        selectedLinePaintRed = new Paint();
+        selectedLinePaintRed.setAntiAlias(true);
+        selectedLinePaintRed.setColor(Color.RED);
+        selectedLinePaintRed.setStyle(Paint.Style.STROKE);
+        selectedLinePaintRed.setStrokeWidth(9);
     }
 
     @Override
@@ -51,60 +67,115 @@ public class PlayGroundView extends View implements View.OnTouchListener {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-        if (rects == null) {
+        if (horizontalLinesOnCanvas == null && verticalLinesOnCanvases == null) {
             int maxValue = Math.min(parentHeight, parentWidth);
-            int squareSideSize = (maxValue - 2) / numberOfSquares;
-            rects = new Rect[numberOfSquares * numberOfSquares];
-            markedRects = new boolean[numberOfSquares * numberOfSquares];
+            int lineLength = (maxValue - 2) / numberOfSquares;
 
-            for (int i = 0; i < numberOfSquares; i++) {
+            horizontalLinesOnCanvas = new lineOnCanvas[(numberOfSquares + 1) * numberOfSquares];
+            verticalLinesOnCanvases = new lineOnCanvas[(numberOfSquares + 1) * (numberOfSquares)];
+
+            for (int i = 0; i < numberOfSquares + 1; i++) {
                 for (int u = 0; u < numberOfSquares; u++) {
-                    rects[i * numberOfSquares + u] = new Rect(
-                            1 + u * squareSideSize,
-                            1 + i * squareSideSize,
-                            1 + u * squareSideSize + squareSideSize,
-                            1 + i * squareSideSize + squareSideSize);
-                    markedRects[i * numberOfSquares + u] = false;
+                    horizontalLinesOnCanvas[u + i * numberOfSquares] = new lineOnCanvas(
+                            1 + u * lineLength,
+                            1 + i * lineLength,
+                            1 + u * lineLength + lineLength,
+                            1 + i * lineLength,
+                            1);
+
+                    verticalLinesOnCanvases[u + i * numberOfSquares] = new lineOnCanvas(
+                            1 + i * lineLength,
+                            1 + u * lineLength,
+                            1 + i * lineLength,
+                            1 + u * lineLength + lineLength,
+                            1);
                 }
             }
         }
 
-        Rect rect;
-        for (int i = 0; i < rects.length; i++) {
-            rect = rects[i];
-            canvas.drawRect(rects[i], mPaint);
-
-//            canvas.drawPoint(rects[i].top,);
-
-
-            if (markedRects[i]) {
-                // Draws the X in a marked Squared
-                canvas.drawLine(rect.top, rect.left, rect.bottom, rect.right, mPaint);
-                canvas.drawLine(rect.top, rect.right, rect.bottom, rect.left, mPaint);
+        if (horizontalLinesOnCanvas != null) {
+            for (lineOnCanvas horizontalLineOnCanvas : horizontalLinesOnCanvas) {
+                canvas.drawLine(horizontalLineOnCanvas.startX, horizontalLineOnCanvas.startY, horizontalLineOnCanvas.stopX, horizontalLineOnCanvas.stopY, linePaintGray);
+                canvas.drawPoint(horizontalLineOnCanvas.startX, horizontalLineOnCanvas.startY, pointPaintWhite);
+                canvas.drawPoint(horizontalLineOnCanvas.stopX, horizontalLineOnCanvas.stopY, pointPaintWhite);
             }
         }
+
+        if (verticalLinesOnCanvases != null) {
+            for (lineOnCanvas verticalLineOnCanvases : verticalLinesOnCanvases) {
+                canvas.drawLine(verticalLineOnCanvases.startX, verticalLineOnCanvases.startY, verticalLineOnCanvases.stopX, verticalLineOnCanvases.stopY, linePaintGray);
+                canvas.drawPoint(verticalLineOnCanvases.startX, verticalLineOnCanvases.startY, pointPaintWhite);
+                canvas.drawPoint(verticalLineOnCanvases.stopX, verticalLineOnCanvases.stopY, pointPaintWhite);
+            }
+        }
+
+        invalidate();
     }
 
+
     @Override
-    public boolean onTouch(View v, MotionEvent event) {
+    public boolean onTouchEvent(MotionEvent event) {
+        // Let the ScaleGestureDetector inspect all events.
+        mScaleDetector.onTouchEvent(event);
+
         float x = event.getX();
         float y = event.getY();
         int i = 0;
 
-        switch (event.getAction()) {
-            case MotionEvent.ACTION_DOWN:
-                for (Rect rect : rects) {
-                    if (rect.left < x && x < rect.right && rect.bottom < y && y < rect.top) {
-                        markedRects[i] = true;
-                        invalidate();
-                        i++;
-                    }
-                }
+        final int action = event.getAction();
+        switch (action & MotionEvent.ACTION_MASK) {
+            case MotionEvent.ACTION_DOWN: {
+
+//                for (Rect rect : rects) {
+//                    if (rect.left < x && x < rect.right && rect.bottom < y && y < rect.top) {
+//                        markedRects[i] = true;
+//                        invalidate();
+//                        i++;
+//                    }
+//                }
+
                 break;
+            }
+
+        }
+        invalidate();
+        return true;
+    }
+
+    private class ScaleListener extends ScaleGestureDetector.SimpleOnScaleGestureListener {
+        @Override
+        public boolean onScale(ScaleGestureDetector detector) {
+            mScaleFactor *= detector.getScaleFactor();
+
+            // Don't let the object get too small or too large.
+            mScaleFactor = Math.max(0.1f, Math.min(mScaleFactor, 10.0f));
+
+            invalidate();
+            return true;
+        }
+    }
+
+    /**
+     * Object class to identify all of the Lines
+     * Changes will still probably come
+     */
+    public class lineOnCanvas {
+        int startX;
+        int startY;
+        int stopX;
+        int stopY;
+        int player;
+
+        lineOnCanvas(int startX, int startY, int stopX, int stopY, int player) {
+            this.startX = startX;
+            this.startY = startY;
+            this.stopX = stopX;
+            this.stopY = stopY;
+            this.player = player;
         }
 
-        return true;
     }
 
 
 }
+
