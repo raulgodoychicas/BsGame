ae0ef4f84e9e9bf278b59a5f082f59fc80fc1a7d
Aaron <aaron.siegl@gmx.de>
2017-02-10 16:21:16 +0100

    Main Activity Foating Button entfernt

--- app/src/main/java/appworld/gogogo/bsgame/MainActivity.java
+++ app/src/main/java/appworld/gogogo/bsgame/MainActivity.java
@@ -31,16 +31,6 @@ public class MainActivity extends Activity {
         setActionBar(toolbar);
 
         switchFragment(new LoginFragment(), this, false);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.hide();
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
